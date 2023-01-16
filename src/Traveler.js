08acb2a8450a0d949b3e351a8aacaaad8bae2991
Adam Hughes <adamjordanhughes@gmail.com>
2023-01-16 17:49:21 -0500

    Format Traveler class file

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,25 +1,25 @@
 class Traveler {
-    constructor (currentTraveler, allTripsData) {
+    constructor(currentTraveler, allTripsData) {
         this.tripsMasterList = allTripsData
         this.userID = currentTraveler.id
         this.name = currentTraveler.name
         this.travelerType = currentTraveler.travelerType
         this.trips = this.getFilteredTrips()
     }
-    
+
     getFilteredTrips() {
-        let filteredTrips = this.tripsMasterList.filter(trip => trip.userID === this.userID )
+        let filteredTrips = this.tripsMasterList.filter(trip => trip.userID === this.userID)
         return filteredTrips
     }
 
     getPastTrips(currentDate) {
-        let pastTrips = this.trips.filter((trip) => currentDate>trip.date)
+        let pastTrips = this.trips.filter((trip) => currentDate > trip.date)
         return pastTrips
     }
 
     getFutureTrips(currentDate) {
-        let futureTrips = this.trips.filter((trip) => currentDate<trip.date && trip.status ==="approved")
-        return futureTrips   
+        let futureTrips = this.trips.filter((trip) => currentDate < trip.date && trip.status === "approved")
+        return futureTrips
     }
 
     getPendingTrips() {
@@ -28,22 +28,22 @@ class Traveler {
     }
 
     getTotalAmountSpentThisYear(year) {
-       let beginningYearDate = new Date (`01/01/${year}`)
-       let endingYearDate = new Date (`12/31/${year}`)
-       
-       let tripsFromThisYear = this.trips.filter((trip) => {
-        if (trip.date > beginningYearDate && trip.date<endingYearDate && trip.status ==="approved") {
-            return true
-        }
-       })
+        let beginningYearDate = new Date(`01/01/${year}`)
+        let endingYearDate = new Date(`12/31/${year}`)
 
-       let sumOfTripCostsFromThisYear = tripsFromThisYear.reduce((acc,trip) => {
+        let tripsFromThisYear = this.trips.filter((trip) => {
+            if (trip.date > beginningYearDate && trip.date < endingYearDate && trip.status === "approved") {
+                return true
+            }
+        })
+
+        let sumOfTripCostsFromThisYear = tripsFromThisYear.reduce((acc, trip) => {
             acc += trip.getTotalTripCost()
             return acc
-       },0)
-       return sumOfTripCostsFromThisYear
-    }
+        }, 0)
 
+        return sumOfTripCostsFromThisYear
+    }
 }
 
 export default Traveler; 
