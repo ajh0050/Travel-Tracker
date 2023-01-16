fcd21735c0c31a7a98575aaa29297777c7483186
Adam Hughes <adamjordanhughes@gmail.com>
2023-01-16 17:14:39 -0500

    Add status code error handling to the GET requests

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,16 +1,26 @@
 function fetchData(dataset) {
     return fetch(`http://localhost:3001/api/v1/${dataset}`)
-    .then( response => {
+    .then( response => {{
+        let responseCode = String(response.status)[0]
         if (response.ok) {
+            console.log(response.status)
             return response.json()
-          } else {
-            throw new Error('The server is down, please try again later')
-          }
+        } else if (responseCode === '4'){
+            console.error(response)
+            alert('Something went wrong, please try again.')
+            throw new Error('4xx level response code error')
+        } else if (responseCode === '5'){
+            console.error(response)
+            alert('The server is down please try again later')
+            throw new Error('5xx level response code error')
+        } else {
+            throw new Error('Something outside of a 4xx or 5xx error happened')
+        }
+    }
     }
       )
     .catch(error => {
         console.log(`${dataset} had an error`, error)
-        // return alert(`The server is down, please try again later`)
     })
 }
 
